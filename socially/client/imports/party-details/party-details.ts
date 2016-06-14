2e8ed2e1d5f92486f7f7aab12b944f3a2495dde4
Dennis Touchet <dtouchet@austin.local>
2016-06-14 12:54:37 -0500

    step 5 complete

--- socially/client/imports/party-details/party-details.ts
+++ socially/client/imports/party-details/party-details.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgZone  } from '@angular/core';
 import { RouteParams } from '@angular/router-deprecated';
+import { Tracker } from 'meteor/tracker';
+import { Parties } from '../../../collections/parties.ts';
 
 @Component({
   selector: 'party-details',
   templateUrl: '/client/imports/party-details/party-details.html'
 })
 export class PartyDetails {
-	constructor(params: RouteParams) {
-		var partyId = params.get('partyId');
-	}
+	party: Object;
 
+	constructor(params: RouteParams, ngZone: NgZone) {
+		var partyId = params.get('partyId');
+
+		Tracker.autorun(() => {
+			ngZone.run(() => {
+				this.party = Parties.findOne(partyId);
+			});
+		});
+	}
 }
