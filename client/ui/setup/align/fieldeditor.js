c74bddd9500940579fb630db77bb2e08bc9c9631
dennistouchet <admin@dennistouchet.com>
2016-12-09 16:11:23 -0600

    Continue working on vertify properties edit capabilities. Nearly complete. Updated some styling and formatting for the code as well. VFY-127

--- client/ui/setup/align/fieldeditor.js
+++ client/ui/setup/align/fieldeditor.js
@@ -1,11 +1,18 @@
 import { Template } from 'meteor/templating';
 import { VertifyProperties } from '../../../../imports/collections/tenant/vertify_property.js'
-
+import { ExternalObjects } from '../../../../imports/collections/tenant/external_object.js'
+import { Systems } from '../../../../imports/collections/tenant/system.js'
 import './fieldeditor.html';
 
 Template.fieldeditor.onCreated(function(){
   Meteor.subscribe('vertify_properties', function (){
     console.log( "Align/FieldEditor - VertifyProperties now subscribed.");
+  });
+  Meteor.subscribe('external_objects', function (){
+    console.log( "Align/FieldEditor - ExternalObjects now subscribed.");
+  });
+  Meteor.subscribe('systems', function (){
+    console.log( "Align/FieldEditor - Systems now subscribed.");
   });
   // Get vertify object id from query parameters in url
   var vo_id = FlowRouter.getQueryParam("id");
@@ -55,10 +62,34 @@ Template.fieldeditor.events({
   }
 });
 
-Template.fieldDetails.helpers({
+Template.fieldeditorproperties.helpers({
+  getFullNameByEOId : function(eo_id){
+    var ws = Session.get("currentWs");
+    var eo = ExternalObjects.findOne(eo_id);
+    let fullname = "";
+    if(eo){
+        var sys = Systems.findOne(eo.system_id);
+        if(sys)
+          fullname = sys.name +"-"+ eo.name;
+    }
+    return fullname;
+  },
+})
 
+Template.fieldeditorfield.helpers({
+  getFullNameByEOId : function(eo_id){
+    var ws = Session.get("currentWs");
+    var eo = ExternalObjects.findOne(eo_id);
+    let fullname = "";
+    if(eo){
+        var sys = Systems.findOne(eo.system_id);
+        if(sys)
+          fullname = sys.name +"-"+ eo.name;
+    }
+    return fullname;
+  },
 });
 
-Template.fieldDetails.events({
+Template.fieldeditorfield.events({
 
 });
